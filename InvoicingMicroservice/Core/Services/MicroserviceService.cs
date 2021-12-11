83358a5a63a2d5ad1c7ec872286f12e3a7757093
skrzypak <hybridPython%13>
2021-12-11 12:05:41 +0100

    Statistic filter

--- InvoicingMicroservice/Core/Services/MicroserviceService.cs
+++ InvoicingMicroservice/Core/Services/MicroserviceService.cs
@@ -30,14 +30,20 @@ namespace InvoicingMicroservice.Core.Services
                 .Include(p => p.DocumentsToProducts)
                     .ThenInclude(d2p => d2p.Document)
                         .ThenInclude(d => d.DocumentType)
-                .Where(p => p.EspId == espId)
+                .Where(p => p.EspId == espId) 
+                .Where(p => productsIds.Count > 0 ? productsIds.Contains(p.Id) : true)
                 .Select(p => new
                 {
                     p.Id,
                     p.Code,
                     p.Name,
                     p.Unit,
-                    Values = p.DocumentsToProducts.Select(d2p => new
+                    Values = p.DocumentsToProducts
+                    .Where(dtp => startDate == null ? true : dtp.Document.Date.Date >= startDate.Value.Date)
+                    .Where(dtp => endDate == null ? true : dtp.Document.Date.Date <= endDate.Value.Date)
+                    .Where(dtp => documentTypesIds.Count > 0 ? documentTypesIds.Contains(dtp.Document.DocumentTypeId) : true)
+                    .Where(dtp => documentStates.Count > 0 ? documentStates.Contains(dtp.Document.State) : true)
+                    .Select(d2p => new
                     {
                         d2p.UnitMeasureValue,
                         d2p.Quantity,
@@ -45,7 +51,9 @@ namespace InvoicingMicroservice.Core.Services
                         d2p.VatValue,
                         d2p.GrossValue
                     })
-                }).Where(px => px.Values.Count() > 0).ToList().GroupBy(px => new { px.Id, px.Code, px.Name }).Select(pxg => new
+                })
+                .Where(px => px.Values.Count() > 0).ToList()
+                .GroupBy(px => new { px.Id, px.Code, px.Name }).Select(pxg => new
                 {
                     pxg.Key,
                     Values = pxg.Select(pxgx => new
@@ -73,6 +81,67 @@ namespace InvoicingMicroservice.Core.Services
 
         public object GetSuppliersProductSummary(int espId, DateTime? startDate, DateTime? endDate, ICollection<int> suppliersIds, ICollection<int> documentTypesIds, ICollection<DocumentState> documentStates, ICollection<int> productsIds)
         {
+
+            object dtos = _context.Documents
+                .AsNoTracking()
+                .Include(doc => doc.Supplier)
+                .Include(doc => doc.DocumentType)
+                .Include(doc => doc.DocumentsToProducts)
+                    .ThenInclude(d2p => d2p.Product)
+                .Where(doc => doc.EspId == espId)
+                .Where(doc => startDate == null ? true : doc.Date >= startDate)
+                .Where(doc => endDate == null ? true : doc.Date <= endDate)
+                .Where(doc => suppliersIds.Count > 0 ? suppliersIds.Contains(doc.SupplierId) : true)
+                .Where(doc => documentTypesIds.Count > 0 ? documentTypesIds.Contains(doc.DocumentTypeId) : true)
+                .Where(doc => documentStates.Count > 0 ? documentStates.Contains(doc.State) : true)
+                .Select(doc => new
+                {
+                    doc.SupplierId,
+                    doc.Supplier.CompanyName,
+                    doc.Supplier.Nip,
+                    doc.Date,
+                    doc.State,
+                    doc.DocumentType,
+                    Products = doc.DocumentsToProducts.Select(d2p => new
+                    {
+                        d2p.ProductId,
+                        d2p.Product.Code,
+                        d2p.Product.Name,
+                        d2p.Product.Unit,
+                        d2p.UnitMeasureValue,
+                        d2p.Quantity,
+                        d2p.NetValue,
+                        d2p.VatValue,
+                        d2p.GrossValue,
+                    }).Where(d2px => productsIds.Count > 0 ? productsIds.Contains(d2px.ProductId) : true).ToList()
+                }).ToList().GroupBy(docx => new { docx.SupplierId, docx.CompanyName, docx.Nip })
+                .Select(group => new
+                {
+                    group.Key,
+                    NumberOfDocuments = group.Count(),
+                    TotalNetValue = group.Sum(g => g.Products.Sum(p => p.NetValue)),
+                    TotalVatValue = group.Sum(g => g.Products.Sum(p => p.VatValue)),
+                    TotalGrossValue = group.Sum(g => g.Products.Sum(p => p.GrossValue)),
+                    Products = group.SelectMany(x =>
+                        x.Products.GroupBy(px => new { px.ProductId, px.Code, px.Name, px.Unit }).Select(d2px => new
+                        {
+                            d2px.Key,
+                            Units = d2px.GroupBy(ux => new { ux.UnitMeasureValue }).Select(uxg => new
+                            {
+                                uxg.Key,
+                                ProductQuantity = uxg.Sum(g => g.Quantity),
+                                ProductNetValue = uxg.Sum(g => g.NetValue),
+                                ProductVatValue = uxg.Sum(g => g.VatValue),
+                                ProductGrossValue = uxg.Sum(g => g.GrossValue),
+                            }),
+                            totalMeasureUnitValue = d2px.Sum(g => g.UnitMeasureValue),
+                            totalNetValue = d2px.Sum(g => g.NetValue),
+                            totaltVatValue = d2px.Sum(g => g.VatValue),
+                            totalGrossValue = d2px.Sum(g => g.GrossValue)
+                        }).ToList()
+                    )
+                }).ToList();
+
             //object dtos = _context.Documents
             //    .Include(doc => doc.Supplier)
             //    .Include(doc => doc.DocumentType)
@@ -140,61 +209,6 @@ namespace InvoicingMicroservice.Core.Services
             //            }).ToList()
             //        )
             //    }).ToList();
-
-            object dtos = _context.Documents
-                .AsNoTracking()
-                .Include(doc => doc.Supplier)
-                .Include(doc => doc.DocumentType)
-                .Include(doc => doc.DocumentsToProducts)
-                    .ThenInclude(d2p => d2p.Product)
-                .Where(doc => doc.EspId == espId)
-                .Select(doc => new
-                {
-                    doc.SupplierId,
-                    doc.Supplier.CompanyName,
-                    doc.Supplier.Nip,
-                    doc.Date,
-                    doc.State,
-                    doc.DocumentType,
-                    Products = doc.DocumentsToProducts.Select(d2p => new
-                    {
-                        d2p.ProductId,
-                        d2p.Product.Code,
-                        d2p.Product.Name,
-                        d2p.Product.Unit,
-                        d2p.UnitMeasureValue,
-                        d2p.Quantity,
-                        d2p.NetValue,
-                        d2p.VatValue,
-                        d2p.GrossValue,
-                    }).ToList()
-                }).ToList().GroupBy(docx => new { docx.SupplierId, docx.CompanyName, docx.Nip })
-                .Select(group => new
-                {
-                    group.Key,
-                    NumberOfDocuments = group.Count(),
-                    TotalNetValue = group.Sum(g => g.Products.Sum(p => p.NetValue)),
-                    TotalVatValue = group.Sum(g => g.Products.Sum(p => p.VatValue)),
-                    TotalGrossValue = group.Sum(g => g.Products.Sum(p => p.GrossValue)),
-                    Products = group.SelectMany(x =>
-                        x.Products.GroupBy(px => new { px.ProductId, px.Code, px.Name, px.Unit }).Select(d2px => new
-                        {
-                            d2px.Key,
-                            Units = d2px.GroupBy(ux => new { ux.UnitMeasureValue }).Select(uxg => new
-                            {
-                                uxg.Key,
-                                ProductQuantity = uxg.Sum(g => g.Quantity),
-                                ProductNetValue = uxg.Sum(g => g.NetValue),
-                                ProductVatValue = uxg.Sum(g => g.VatValue),
-                                ProductGrossValue = uxg.Sum(g => g.GrossValue),
-                            }),
-                            totalMeasureUnitValue = d2px.Sum(g => g.UnitMeasureValue),
-                            totalNetValue = d2px.Sum(g => g.NetValue),
-                            totaltVatValue = d2px.Sum(g => g.VatValue),
-                            totalGrossValue = d2px.Sum(g => g.GrossValue)
-                        }).ToList()
-                    )
-                }).ToList();
 
             if (dtos is null)
             {
